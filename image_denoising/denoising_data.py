7b4247630652a9bf00ac5a66aef9d6f61c870020
lijia666 <li638354@163.com>
2026-03-22 09:42:38 +0800

    Update denoising_data.py

--- image_denoising/denoising_data.py
+++ image_denoising/denoising_data.py
@@ -16,9 +16,9 @@ def sorted_alphanum(img_names):
     而文件名是个字符串，排序后与整数1，2，3....不再对应，
     因此此方法对文件名进行排序，使其字母部分按字母排序，
     数字部分按整数规则排序
-    
-    :param img_names: 
-    :return: 
+
+    :param img_names:
+    :return:
     """
     convert = lambda x: int(x) if x.isdigit() else x.lower()
     # split根据数字进行分割，且保留数组，返回一个列表，列表中元素为数字和字母
@@ -33,8 +33,8 @@ class ImageDataset(Dataset):
     实现分批获取图片
     """
     def __init__(self, image_dir, transform=None):
-        self.main_dir = image_dir # 图片所在目录，方便后面拼接完整图片路径
-        self.transform = transform # 数据转换，将Image数据转为tensor数据
+        self.main_dir = image_dir# 图片所在目录，方便后面拼接完整图片路径
+        self.transform = transform# 数据转换，将Image数据转为tensor数据
         #在后面获取图片时，根据main.dir再拼接图片名，获取图片
         self.image_names = sorted_alphanum(os.listdir(image_dir))  # 获取目录下所有图片文件名，保存为列表
 
@@ -49,12 +49,16 @@ class ImageDataset(Dataset):
         # 打开图片
         #由于Image.open()打开图片时，图片带一个透明色(RGBA)，即四个维度，而此数据集图片是三维度，因此要转为三维
         image = Image.open(image_path).convert('RGB')
-        if self.transform is not None: 
+        if self.transform is not None:
             tensor_image = self.transform(image)  # 范围在0-1之间--ToTensor() 会将像素值从 [0, 255] 映射到 [0.0, 1.0]
         else:  # 如果没有定义转换，引出异常
             raise ValueError('No transform')
         # 向原始图像中手动加一些噪声
-        noise_img = tensor_image + NOISE_FACTOR * torch.randn_like(tensor_image)
+        # noise_img = tensor_image + NOISE_FACTOR * torch.randn_like(tensor_image)
+        # #添加高斯噪声
+        noise_img = torch.randn_like(tensor_image) * NOISE_FACTOR
+        noise_img = tensor_image + noise_img
+
         noise_img = torch.clamp(noise_img, 0., 1.)  # 剪切到0-1之间,因为加上噪声
 
         # 返回噪声图片和原始图片

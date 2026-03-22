10f21d242c30dc185b9660a10216de1a47f43d1e
lijia666 <li638354@163.com>
2026-03-22 09:42:54 +0800

    Update denoising_config.py

--- image_denoising/denoising_config.py
+++ image_denoising/denoising_config.py
@@ -1,8 +1,8 @@
 #数据预处理配置
-IMG_PATH='../common/dataset/'
+IMG_PATH='../common/BSD68/'
 #原始图片大小
-IMG_HEIGHT=68
-IMG_WIDTH=68
+IMG_HEIGHT=64
+IMG_WIDTH=64
 
 #随机性和数据集划分
 SEED=42
@@ -10,13 +10,13 @@ TRAIN_RATIO=0.75
 TEST_RATIO=0.25
 
 #随机噪声系数
-NOISE_FACTOR=0.5
+NOISE_FACTOR=0.06
 
 #训练相关超参
 #学习率
 LEARNING_RATE=0.001
 #训练轮数
-EPOCHS=30
+EPOCHS=60
 #批次大小
 BATCH_SIZE=32
 
